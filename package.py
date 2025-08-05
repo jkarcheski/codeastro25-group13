e4bec6bb3e704a3f47043cf4a680dee2bd86cd90
taliaoshea <taliaoshea@gmail.com>
2025-08-05 14:03:12 -0500

    updating skeleton structure of code

--- package.py
+++ package.py
@@ -6,3 +6,35 @@ from astropy.table import Table, unique, hstack  # type: ignore
 from astropy.io import fits, ascii  # type: ignore
 import os
 import mplcursors
+
+"""
+DEFINING STAGE 
+"""
+# function: get_sdss_data(number_of_files)
+
+# define classes:
+# overall astronomical object class
+
+### subclass of AGN
+
+### ### AGN class init:
+### ### ### read in fits file & define self.
+
+# function: interactive plotting(AGN_object_list, paramx, paramy)
+
+#
+
+"""
+RUNNING STAGE
+
+
+get_sdss_data(nfiles = )
+
+agn_object_list = []
+for loop thru nfiles:
+    agn = AGN(file)
+    agn_object_list.append(agn)
+
+interactive_plot(agn_object_list, plotparamx, plotparamy)
+    
+"""
