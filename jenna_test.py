3eded27358ad9e64a5a30ef4a169d84d7262c34a
taliaoshea <taliaoshea@gmail.com>
2025-08-05 09:40:43 -0500

    adding print statement to test branching

--- jenna_test.py
+++ jenna_test.py
@@ -2,14 +2,16 @@ import matplotlib.pyplot as plt
 import numpy as np
 import mplcursors
 
+print("Plotting function document")
+
 # Generate mock data
 num_points = 10
 x = np.random.rand(num_points)
 y = np.random.rand(num_points)
 
-# Create the plotting environment 
-fig, ax = plt.subplots(1,2, figsize = (10, 4))
-scatter = ax[0].scatter(x,y, color='k')
+# Create the plotting environment
+fig, ax = plt.subplots(1, 2, figsize=(10, 4))
+scatter = ax[0].scatter(x, y, color="k")
 ax[0].set_title("Main Interactive Plot")
 ax[1].set_title("Spectrum")
 cursor_click = mplcursors.cursor(scatter, hover=False)  # or just mplcursors.cursor()
@@ -26,9 +28,11 @@ cursor_hover = mplcursors.cursor(scatter, hover=2)
 # extras: an additional list of artists, that will be removed whenever the main annotation is deselected.
 ########################################################################################################
 
+
 def on_hover(sel):
     # TODO: replace 'sel.index' with AGN name
     sel.annotation.set_text(sel.index)
+
 
 def on_click(sel):
     # generates a new plot in the other panel
@@ -36,11 +40,13 @@ def on_click(sel):
     ax[1].set_title("Spectrum")
 
     sel.annotation.set_text("")
-    ax[0].scatter(x = sel.target[0], y = sel.target[1], color='r')
-    ax[1].scatter(x,y)
+    ax[0].scatter(x=sel.target[0], y=sel.target[1], color="r")
+    ax[1].scatter(x, y)
+
 
 def on_remove(sel):
-    ax[0].scatter(x = sel.target[0], y = sel.target[1], color='k')
+    ax[0].scatter(x=sel.target[0], y=sel.target[1], color="k")
+
 
 cursor_hover.connect("add", on_hover)
 cursor_click.connect("add", on_click)
